b90cd014dfd8722151b5096a95d0c9a51cb77291
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-10-11 02:47:27 +0000

    Remove Blaze dependency

--- app/main_client.js
+++ app/main_client.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import App from './components/App.jsx';
 
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY',
-});
-
 console.log('Running on client only');
 
 Meteor.startup(() => {
